52d0eade9ce425b164f768526981d7458cb2adcb
liberatrice bayizere <92096502+liberatricebaizer@users.noreply.github.com>
2023-03-02 12:54:13 +0200

    Initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My personal portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of javaScript Mastery is to help aspiring and established
+        developers to take their development skills to the next level and build
+        owesome apps.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
